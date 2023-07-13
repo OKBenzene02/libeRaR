e765686470febb986125d4d4cca89a41d9ce86ac
OKBenzene02 <liyakhatyousuf@gmail.com>
2023-07-13 13:44:16 +0530

    second commit

---
+++ routes/authors.js
@@ -0,0 +1,50 @@
+const { Router } = require("express");
+const Author = require('../models/author')
+
+const express = require('express');
+const author = require("../models/author");
+const router = express.Router();
+
+// Route for All Authors
+router.get('/', async (req, response) => {
+    let searchAuthor = {};
+    if (req.query.name !== null && req.query.name !== ''){
+        searchAuthor.name = new RegExp(req.query.name, 'i')
+    }
+
+    try {
+        const authors = await Author.find(searchAuthor)
+        response.render('authors/index', { authors: authors, 
+            searchAuthor: req.query });
+    }
+    catch {
+        response.redirect('/')
+    }
+
+
+})
+
+// Route for New Authors
+router.get('/new', (req, response) => {
+    response.render('authors/new', { author: new Author() });
+})
+
+// Route for Author Route
+router.post('/', async (req, response) =>{
+    const author = new Author({
+        name: req.body.name,
+    })
+
+    try {  
+        const newAuthor = await author.save();
+        response.redirect('authors')
+    }
+    catch{  
+        response.render('authors/new', {
+            author: author,
+            errMessage: "Error creating Author"
+        })
+    }
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -6,19 +6,28 @@ const express = require('express');
 const app = express(); 
 const expressLayouts = require('express-ejs-layouts');
 
-const indexRouter = require('./routes/index')
-const mongoose = require('mongoose')
+const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
+
+const bodyparser = require('body-parser');
+
+// Connection with mongoDB database
+const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('Connected to mongoose'))
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to mongoose'));
 
-
+// Static Express JS files should be included when creating a new application
 app.set('view engine', 'ejs');
 app.set("views", __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
-app.use('/', indexRouter)
+
+app.use(bodyparser.urlencoded({ limit: '10mb', extended: false }))
+app.use('/', indexRouter);
+app.use('/authors', authorRouter);
+
 
 app.listen(process.env.PORT || 3000);
